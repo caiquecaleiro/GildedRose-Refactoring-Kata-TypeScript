17579ddcd2973f981d8c249e286a2cfb303a68e3
Caique Caleiro <caiquecaleiro@gmail.com>
2023-10-14 13:17:16 +1300

    Add Aged Brie test cases - 09:00

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,54 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+describe("Gilded Rose", () => {
+	describe("Aged Brie", () => {
+		describe("should increase quality", () => {
+			it("by 1 when getting older", () => {
+				const agedBrie = new Item("Aged Brie", 2, 0);
+				const gildedRose = new GildedRose([agedBrie]);
+
+				gildedRose.updateQuality();
+
+				const [updatedAgedBrie] = gildedRose.items;
+
+				expect(updatedAgedBrie.quality).toBe(1);
+			});
+
+			// Double check if this is intentional or bug, current code does that.
+			it("by 2 when sell in has passed (negative)", () => {
+				const agedBrie = new Item("Aged Brie", 0, 5);
+				const gildedRose = new GildedRose([agedBrie]);
+
+				gildedRose.updateQuality();
+
+				const [updatedAgedBrie] = gildedRose.items;
+
+				expect(updatedAgedBrie.quality).toBe(7);
+			});
+
+			it("up to the limit of 50", () => {
+				const agedBrie = new Item("Aged Brie", 1, 50);
+				const gildedRose = new GildedRose([agedBrie]);
+
+				gildedRose.updateQuality();
+
+				const [updatedAgedBrie] = gildedRose.items;
+
+				expect(updatedAgedBrie.quality).toBe(50);
+			});
+		});
+
+		describe("should decrease sell in", () => {
+			it("by 1 when getting older", () => {
+				const agedBrie = new Item("Aged Brie", 2, 0);
+				const gildedRose = new GildedRose([agedBrie]);
+
+				gildedRose.updateQuality();
+
+				const [updatedAgedBrie] = gildedRose.items;
+
+				expect(updatedAgedBrie.sellIn).toBe(1);
+			});
+		});
+	});
 });
